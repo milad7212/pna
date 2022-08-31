5ad24c180ca367619cc3fafdf95cf6be99f40eb6
miillaadddddd <milad7212@gmail.com>
2022-08-31 21:34:52 +0430

    init

---
+++ pages/index.jsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Image from "next/image";
+import About from "../components/About";
+
+export default function Home() {
+  return (
+    <div className="">
+      {/* top header */}
+
+      {/* header */}
+
+      {/* hero slider */}
+
+      {/* about */}
+      <About />
+
+      {/* information */}
+
+      {/* roles */}
+
+      {/* history */}
+
+      {/* Organizer */}
+
+      {/* supporters */}
+
+      {/* videos */}
+
+      {/* images */}
+
+      {/* footer */}
+    </div>
+  );
+}
