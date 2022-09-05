6456345f36e4396fd2207907eca907ca6deb48cb
miillaadddddd <milad7212@gmail.com>
2022-09-05 16:40:41 +0430

    roles page

---
+++ pages/roles.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function roles() {
+  return (
+    <>
+      <div className="px-4 mx-auto my-8 max-w-7xl">
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
+    </>
+  );
+}
+
+export default roles;
+
+function Item() {
+  return (
+    <>
+      <p className="my-8 font-bold">نرم افزار موبایل</p>
+      <p className="leading-7 text-justify">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab laboriosam
+        optio facere delectus nihil quos expedita, natus sapiente illum sed
+        consequuntur quidem. Maxime animi reiciendis sunt dolorem quam,
+        recusandae minima! Maiores magni repudiandae explicabo voluptatum nihil
+        ad odio nemo molestiae ab in sunt, nesciunt amet, aperiam minus. Fugiat
+        id quia a, enim laudantium itaque recusandae modi officiis est
+        temporibus hic! Quos eum nostrum inventore esse soluta tempora ea
+        explicabo asperiores, cumque quasi est deleniti rem at? Quibusdam
+        distinctio delectus suscipit nisi, blanditiis quis, pariatur facilis
+        iste id natus cupiditate accusamus. Doloribus, voluptatem rem alias
+        quisquam vitae perferendis, molestiae debitis saepe quibusdam adipisci,
+        ex cum? Sed, hic cupiditate! Mollitia beatae tempore quia in nihil
+        placeat voluptate maiores sit. Qui, reiciendis nesciunt. Assumenda
+        corporis recusandae voluptatem optio quo quia, dicta commodi non cum
+        repellat ipsum iste eum quibusdam, dolorum sint esse soluta
+        exercitationem magni placeat at illo dignissimos officiis accusamus?
+        Cumque, mollitia!
+      </p>
+    </>
+  );
+}
