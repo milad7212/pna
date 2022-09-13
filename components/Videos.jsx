0240b355afb9d734789aa96b79547e802aff29f0
miillaadddddd <milad7212@gmail.com>
2022-09-13 16:49:41 +0430

    set video api

--- components/Videos.jsx
+++ components/Videos.jsx
@@ -1,17 +1,47 @@
 import Image from "next/image";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { http } from "../services/api";
 function Videos() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await http.get(
+        " https://event.bsjmajazi.ir/api/gallery/videos/",
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.status < 400) {
+        setVideos(response.data);
+      }
+      console.log("response :>> ", response);
+    }
+
+    fetchData();
+  }, []);
   return (
     <div className="px-2 my-8 md:px-20">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <div className="">
           <p className="my-8 text-xl font-bold">پخش زنده / کلیپ ها</p>
-          <Item />
-          <Item />
-          <Item />
+          {videos.map((item, index) => (
+            <>
+              <Item data={item} />
+            </>
+          ))}
         </div>
-        <div className="bg-gray-500 w-[70%] aspect-square mx-auto rounded-lg shadow-md"></div>
+        <div className=" w-[70%]  mx-auto rounded-lg shadow-md">
+          {videos[0]?.video && (
+            <video
+              controls
+              className="aspect-square"
+              type="video/mp4"
+              src={videos[0]?.video}
+            ></video>
+          )}
+        </div>
       </div>
     </div>
   );
@@ -19,15 +49,15 @@ function Videos() {
 
 export default Videos;
 
-function Item() {
+function Item({ data }) {
   return (
     <div className="grid   grid-cols-[50px_1fr_80px_100px] bg-gray-400 my-2 rounded-xl p-2">
       <div className="">
-        <Image src="/icon.png" layout="fixed" width={50} height={50} alt="" />
+        <Image src={data.icon} layout="fixed" width={50} height={50} alt="" />
       </div>
-      <p className="mr-2 place-self-center">فیلم</p>
-      <p className=" place-self-center">25:12</p>
-      <div className="flex place-self-center">icons</div>
+      <p className="mr-2 place-self-center">{data.title}</p>
+      <p className=" place-self-center">{data.duration}</p>
+      <div className="flex place-self-center"></div>
     </div>
   );
 }
